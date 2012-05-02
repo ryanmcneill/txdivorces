0325ff52bef27a33de7444e506887aac88a2bbc3
Ryan McNeill <rmcneill@dallasnews.com>
2012-05-02 14:26:13 -0500

    changed save dir to variable so user can chg easily

--- TexasDivorces.rb
+++ TexasDivorces.rb
@@ -2,6 +2,9 @@
 require 'mechanize'
 require 'open-uri'
 
+#change this to the directory where you'd like to store the files
+my_directory = 'E:/Data/DSHS/Divorces/'
+
 my_agent = Mechanize.new
 
 #this is the home of the Texas divorces index
@@ -11,9 +14,10 @@ my_page = my_agent.get "http://www.dshs.state.tx.us/vs/marriagedivorce/dindex.sh
 #it then saves each one to your hard drive
 my_page.links_with(:text => /\d\d\d\d/).each do |link|
   the_link = 'http://www.dshs.state.tx.us/' + "#{link.uri}"
-  file_name = "#{link.text}" + ".zip"
+  file_name = "#{my_directory}" + "#{link.text}" + ".zip"
   puts "Downloading #{link.text}...and saving it as #{file_name}"
   open("#{file_name}", 'wb') do |fo|
     fo.print open("#{the_link}").read
   end
 end
+
